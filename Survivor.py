7ef5c41fa62f56eee41337826f9831b1db859382
Gabriel Antão <gb.antao@gmail.com>
2014-07-18 19:59:31 -0300

    Weapon-Wall Collision, drawing weapons in right position.

--- Survivor.py
+++ Survivor.py
@@ -68,26 +68,29 @@ class Survivor(Character):
 
     def draw(self, screen):
 
-        screen.blit(self.img, (self.x, self.y))
-
-        h = self.width / 2
         img = Survivor.GUN_IMAGES[self.current_weapon]
 
         if self.direction == Direction.WEST:
-            screen.blit(img, (self.x, self.y + h))
+            screen.blit(self.img, (self.x, self.y))
+            screen.blit(img, (self.centerx - img.get_rect().width, self.centery))
 
         elif self.direction == Direction.EAST:
+            screen.blit(self.img, (self.x, self.y))
             img = pygame.transform.flip(img, True, False)
-            screen.blit(img, (self.x + h, self.y + h))
+            screen.blit(img, (self.centerx, self.centery))
 
         elif self.direction == Direction.SOUTH:
+            screen.blit(self.img, (self.x, self.y))
             img = pygame.transform.rotate(img, 90)  # CCW
-            screen.blit(img, (self.x + h, self.y + h))
+            screen.blit(img, (self.centerx, self.centery))
 
         elif self.direction == Direction.NORTH:
             south = pygame.transform.rotate(img, 90)
             img = pygame.transform.flip(south, False, True)
-            screen.blit(img, (self.x + h, self.y - h))
+            screen.blit(img, (self.centerx, self.centery - img.get_rect().height))
+            screen.blit(self.img, (self.x, self.y))
+
+
 
     """ Changes player direction iamge"""
     def rotate(self, direction):
